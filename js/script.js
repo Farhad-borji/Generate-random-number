bf647de31d0dbc034692131c68aa3e6aa3f53bac
farhad borji <fborji10@gmail.com>
2024-08-18 01:04:48 +0330

    finished

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
-const minimum = document.getElementById("minimum")
-const maximum = document.getElementById("maximum")
-const generateButton = document.querySelector("button")
-console.log(generateButton);
-
+const minimum = document.getElementById("minimum");
+const maximum = document.getElementById("maximum");
+const generateButton = document.querySelector("button");
+const generatedNum = document.getElementById("random-number");
 
 const generateHandler = () => {
-  
-}
+  const min = +minimum.value;
+  const max = +maximum.value;
+  if (!max) {
+    generatedNum.innerText = "Need to enter the Maximum";
+  } else {
+    const randomNum = min + Math.trunc(Math.random() * (max - min + 1));
+    generatedNum.innerText = `Random number : ${randomNum}`;
+  }
+};
 
-generateButton = addEventListener("click", generateHandler)
+generateButton.addEventListener("click", generateHandler);

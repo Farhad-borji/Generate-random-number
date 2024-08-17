1d2d1167d966f205690cbcb6364a462b7abbc97d
farhad borji <fborji10@gmail.com>
2024-08-18 00:44:28 +0330

    html and css completed

--- js/script.js
+++ js/script.js
@@ -1,31 +1,11 @@
-const inputElement = document.getElementById("date");
-const weekDay = document.getElementById("week-day");
-let inputTimeout;
+const minimum = document.getElementById("minimum")
+const maximum = document.getElementById("maximum")
+const generateButton = document.querySelector("button")
+console.log(generateButton);
 
-const getWeekday = () => {
-  const daysOfWeek = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  const inputValue = inputElement.value;
-  const date = new Date(inputValue);
-  const dayIndex = date.getDay();
-  const day = daysOfWeek[dayIndex]
-  if (!dayIndex) {
-    weekDay.innerText = "Error : Invalid date"
-  } else {
-    weekDay.innerText = `It's ${day}`
-  }
-};
 
-const inputHandler = () => {
-  clearTimeout(inputTimeout);
-  inputTimeout = setTimeout(getWeekday, 1000);
-};
+const generateHandler = () => {
+  
+}
 
-inputElement.addEventListener("input", inputHandler);
+generateButton = addEventListener("click", generateHandler)
